02f429a821f2de83019ec896773d6d026deaa8b3
edg <dussert.girard@gmail.com>
2016-09-06 16:35:17 +0200

    Modeule 5 done, basic game finished

--- ninegame.jsx
+++ ninegame.jsx
@@ -1,4 +1,24 @@
   // Code goes here
+  
+  var  possibleCombinationSum = function(arr, n) {
+  if (arr.indexOf(n) >= 0) { return true; }
+  if (arr[0] > n) { return false; }
+  if (arr[arr.length - 1] > n) {
+    arr.pop();
+    return possibleCombinationSum(arr, n);
+  }
+  var listSize = arr.length, combinationsCount = (1 << listSize)
+  for (var i = 1; i < combinationsCount ; i++ ) {
+    var combinationSum = 0;
+    for (var j=0 ; j < listSize ; j++) {
+      if (i & (1 << j)) { combinationSum += arr[j]; }
+    }
+    if (n === combinationSum) { return true; }
+  }
+  return false;
+  };
+  
+  
 var StarsContainer = React.createClass({
   render : function(){
     var stars = []
@@ -22,11 +42,39 @@ var StarsContainer = React.createClass({
 
 var ButtonContainer = React.createClass({
   render : function(){
+    
+    switch(this.props.correct){
+      case true:
+        var button = (
+          <button className="btn btn-success" onClick={this.props.acceptAnswer}> 
+          <span className="glyphicon glyphicon-ok"></span>
+          </button>
+      )
+      break;
+      case false:
+      var button = (
+          <button className="btn btn-danger">
+          <span className="glyphicon glyphicon-remove"></span>
+      </button>)
+      break;
+      default:
+      var disabled = this.props.selectedNumbers.length == 0 ? "disabled" : "" ;
+      var button = (<button className="btn btn-primary" onClick={this.props.checkAnswer} disabled={disabled}>
+        =
+      </button>)
+      break;
+    }
+    
+    var disableRedraw = this.props.numberOfTryLeft == 0 ? "disabled" : "" ;
+    
     return (
       <div id="button-container">
-      <button className="btn btn-primary" onClick={this.props.checkAnswer}>
-        {this.props.numberOfTryLeft}
-      </button>
+      {button} 
+      <div>
+        <button className="btn btn-warning btn-xs" disabled={disableRedraw} onClick={this.props.redraw}>
+          <span className="glyphicon glyphicon-refresh">&nbsp;{this.props.numberOfTryLeft}</span>
+        </button>
+      </div>
       </div>
       )
   }
@@ -61,13 +109,26 @@ var NumberContainer = React.createClass({
     var numberDivs = [];
     var className;
     var selectedNumbers = this.props.selectedNumbers;
+    var usedNumbers = this.props.usedNumbers;
+    
 
     for(var i=1; i <=9; i++)
     {
+      var isUsed = (usedNumbers.indexOf(i) >=0);
       className = "number selected-" + (selectedNumbers.indexOf(i) >=0);
+      className += " used-" + isUsed;
+      
+      if(!isUsed)
+      {
       numberDivs.push(
         <div className={className} onClick={this.props.selectNumber.bind(null,i)}>{i}</div>
         )
+      }
+      else{
+        numberDivs.push(
+        <div className={className}>{i}</div>
+        )
+      }
     }
 
     return (
@@ -80,48 +141,132 @@ var NumberContainer = React.createClass({
   }
 });
 
+var DoneContainer = React.createClass({
+  render: function(){
+    return (
+      <div id="done-container" className="well text-cemter">
+        <h2>{this.props.doneStatus}</h2>
+      </div>
+      )
+  }
+});
+
 
 var Game = React.createClass({
   getInitialState : function(){
     return {
       selectedNumbers:[],
-      numberOfStars : Math.floor(Math.random()*9)+1,
-      numberOfTryLeft : 3
+      numberOfStars : this.randomNumber(),
+      numberOfTryLeft : 5,
+      correct: null,
+      usedNumbers : [],
+      doneStatus : null
     };
+  },
+  randomNumber : function(){
+    return Math.floor(Math.random()*9)+1;
   },
   selectNumber : function(clicked){
     if(this.state.selectedNumbers.indexOf(clicked) < 0 )
     {
-      this.setState({selectedNumbers : this.state.selectedNumbers.concat(clicked)});
+      this.setState(
+        {
+        selectedNumbers : this.state.selectedNumbers.concat(clicked), 
+        correct:null
+      });
     } 
   },
   unselectNumber : function(unclicked){
     var index = this.state.selectedNumbers.indexOf(unclicked);
-    console.log(index);
-    
+
     if(index >= 0)
     {
-      console.log(unclicked);
       var t = this.state.selectedNumbers.splice(index,1);
-      console.log(t);
-      this.setState({selectedNumbers : this.state.selectedNumbers});
-      console.log(this.state.selectedNumbers);
+      this.setState(
+        {
+        selectedNumbers : this.state.selectedNumbers,        
+        correct:null
+          
+        });
     }
       
   },
   checkAnswer : function(){
-    console.log("check answer");
     var sumAnswer =  this.state.selectedNumbers.reduce(function(pv, cv) { return pv + cv; }, 0);
-    if(this.state.numberOfStars == sumAnswer)
+    var correct = this.state.numberOfStars === sumAnswer;
+    this.setState({correct: correct});
+  },
+  acceptAnswer : function(){
+    var usedNumbers = this.state.usedNumbers.concat(this.state.selectedNumbers);
+    this.setState({
+      usedNumbers:usedNumbers,
+      correct : null,
+      selectedNumbers: [],
+      numberOfStars : this.randomNumber()
+    }, function(){
+      this.updateDoneStatus()
+    });
+  },
+  redraw : function(){
+    if(this.state.numberOfTryLeft > 0)
     {
-      console.log("good");
-    }
-    else{
-      this.setState({numberOfTryLeft :this.state.numberOfTryLeft-1});
-      console.log("tries left:" + this.state.numberOfTryLeft);
+      this.setState({ selectedNumbers:[],
+          numberOfStars : this.randomNumber(),
+          correct: null,
+          numberOfTryLeft: this.state.numberOfTryLeft-1 
+      }, function(){
+      this.updateDoneStatus()
+    });
     }
   },
+   possibleSolutions: function(){
+    var numberOfStars = this.state.numberOfStars, 
+        possibleNumbers= [],
+        usedNumbers = this.state.usedNumbers;
+   
+    for(var i = 1; i < 10; i++)
+    {
+      if(usedNumbers.indexOf(i) < 0)
+      {
+        possibleNumbers.push(i); 
+      }
+    }
+    var t = possibleCombinationSum(possibleNumbers,numberOfStars); 
+    return t; 
+  },
+  updateDoneStatus: function(){
+    console.log("tries left:" + (this.state.numberOfTryLeft));
+    if(this.state.usedNumbers.lenght === 9){
+      this.setState({doneStatus:"YOU WON!"})
+      return;
+    }
+    console.log(!this.possibleSolutions() && this.state.numberOfTryLeft <= 0);
+    if(!this.possibleSolutions() && this.state.numberOfTryLeft <= 0)
+    {
+      this.setState({doneStatus:"Game over!"})
+      return;
+    }
+    
+  },
   render : function(){
+    var selectedNumbers = this.state.selectedNumbers,
+    bottomContainer;
+    
+    if(this.state.doneStatus)
+    {
+      bottomContainer = (
+         < DoneContainer doneStatus={this.state.doneStatus} />
+        )
+    }
+    else
+    {
+      bottomContainer = (
+        <NumberContainer selectedNumbers={this.state.selectedNumbers} 
+                         usedNumbers={this.state.usedNumbers}
+                        selectNumber={this.selectNumber}/>
+        )
+    }
+    
     return(
       <div id="game">
       <h2>The game</h2>
@@ -129,14 +274,18 @@ var Game = React.createClass({
         <div className="clearfix">
           < StarsContainer numberOfStars={this.state.numberOfStars} />
           < ButtonContainer numberOfTryLeft={this.state.numberOfTryLeft}
-                            checkAnswer={this.state.checkAnswer}/>
-          < AnswerContainer selectedNumbers={this.state.selectedNumbers}
+                            checkAnswer={this.checkAnswer}
+                            acceptAnswer={this.acceptAnswer}
+                            selectedNumbers={selectedNumbers}
+                            correct={this.state.correct}
+                            redraw={this.redraw}
+                            />
+          < AnswerContainer selectedNumbers={selectedNumbers}
                             unselectNumber={this.unselectNumber}/> 
         </div>
-        
-        <NumberContainer selectedNumbers={this.state.selectedNumbers} 
-                        selectNumber={this.selectNumber}/>
-        
+
+       {bottomContainer}
+
       </div>
       )
   }
